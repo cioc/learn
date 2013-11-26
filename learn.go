6e3098bb9aa6880bd32e13d7496195a94adad7fb
Charles Cary <cclego@gmail.com>
2013-11-25 22:56:36 -0600

    feature scaling and mean normalization

--- learn.go
+++ learn.go
@@ -6,7 +6,6 @@ import (
   "github.com/cioc/learn/optimize"
   "fmt"
 )
-
 func main() {
   features, vals, err := input.Read()
   if err != nil {
@@ -18,11 +17,25 @@ func main() {
     fmt.Println(vals[i])
   }
   //TODO - NUMBER # PARAMS MUST MATCH # FEATURES
-  lin := linear.New([]float64{0.0,0.0}, features, vals)
-  for i := 0; i < 1500; i++ {
+  //TODO - CLEAN UP ALL THIS GARBAGE
+  lin := linear.New([]float64{0.0,0.0,0.0}, features, vals)
+  means, ranges := lin.ScaleFeatures()
+  
+  //fmt.Println(means)
+  //fmt.Println(ranges)
+  lin.PrintData()
+  for i := 0; i < 100000; i++ {
     fmt.Println(lin.Cost())
     params2 := optimize.GradientDescent(lin, 0.01)
     fmt.Println(params2)
     lin.SetParams(params2)
   }
+  finalParams := lin.Params()
+  fmt.Println(finalParams)
+  fmt.Println(means)
+  fmt.Println(ranges)
+  for i := range(finalParams) {
+    fmt.Println(finalParams[i] / ranges[i])
+  }
+  fmt.Println(finalParams[0] + (finalParams[1] * ((1650.0 - means[1]) / ranges[1])) + (finalParams[2] * ((3.0 - means[2]) / ranges[2])))
 }
--- model/linear/linear.go
+++ model/linear/linear.go
@@ -1,5 +1,9 @@
 package linear
 
+import (
+  "fmt"
+)
+
 type Linear struct {
   params []float64
   samples [][]float64
@@ -12,23 +16,58 @@ func New(params []float64, samples [][]float64, labels []float64) (Linear) {
 
 func dot(as []float64, bs []float64) (float64) {
   var sum float64
-  sum = 0
   for i := range(as) {
     sum += as[i] * bs[i]
   }
   return sum
 }
 
+func (model Linear) PrintData() {
+  for i := range(model.samples) {
+    fmt.Print(model.samples[i])
+    fmt.Print(" - ")
+    fmt.Println(model.labels[i])
+  }
+}
+
+func (model Linear) ScaleFeatures() (means []float64, ranges []float64) {
+  featureCount := len(model.params)
+  sampleCount := len(model.labels)
+  means = make([]float64, featureCount, featureCount)
+  ranges = make([]float64, featureCount, featureCount)
+  for i := 1; i < featureCount; i++ {
+    //determine mean and range
+    var mean float64
+    max := model.samples[0][i]
+    min := model.samples[0][i]
+    for j := 0; j < sampleCount; j++ {
+      mean += (1 / float64(sampleCount)) * model.samples[j][i]
+      if model.samples[j][i] > max {
+        max = model.samples[j][i]
+      }
+      if model.samples[j][i] < min {
+        min = model.samples[j][i]
+      }
+    }
+    for j := 0; j < sampleCount; j++ {
+      model.samples[j][i] -= mean
+      model.samples[j][i] /= (max - min)
+    }
+    means[i] = mean
+    ranges[i] = (max - min)
+  }
+  return means, ranges
+}
+
 func (model Linear) Cost() (float64) {
   m := float64(len(model.labels))
   var accum float64
-  accum = 0
   for i := range(model.samples) {
     err := dot(model.samples[i], model.params) - model.labels[i]
     err *= err
-    accum += err
+    accum += (1 / (2 * m)) * err
   }
-  return (1 / (2 * m)) * accum
+  return accum
 }
 
 func (model Linear) Params() ([]float64) {
@@ -40,13 +79,13 @@ func (model Linear) SetParams(params []float64) {
 }
 
 func (model Linear) Gradient(delta int) (float64) {
-  m :=  float64(len(model.labels))
+  m := float64(len(model.labels))
   var accum float64
-  accum = 0
   for i := range(model.samples) {
     err := dot(model.samples[i], model.params) - model.labels[i]
+    err *= (1 / m)
     err *= model.samples[i][delta]
     accum += err
   }
-  return (1 / m) * accum
+  return accum
 }
